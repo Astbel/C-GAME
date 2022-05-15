4abcc9ea706d56b7e5b9a6d00fa9697e157b0685
astbel <astbel4633214@gmail.com>
2022-05-15 22:21:20 +0800

    C# 地下城 移動方法建立好

---
+++ dungeon/RPG/RPG/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace RPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = ((System.Drawing.Image)(resources.GetObject("Player.Image")));
+            this.Player.Location = new System.Drawing.Point(121, 100);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(50, 50);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Event);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ dungeon/RPG/RPG/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPG
+{
+    public partial class Form1 : Form
+    {
+        //變數
+        bool MoveRight, MoveLeft, MoveUp, MoveDown;
+
+        int speed = 12;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Point location = Player.Location;
+            Console.WriteLine(location.ToString());
+            //timer1.Start();
+            //this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Keyisdown);
+            //this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Keyisup);
+        }
+
+        private void Timer_Event(object sender, EventArgs e)
+        {
+            Point location = Player.Location;
+            Console.WriteLine(location.ToString());
+            if (MoveLeft == true && Player.Left > 0)
+            {
+                Player.Left -= speed;
+            }
+            if (MoveRight == true && Player.Right < 818)
+            {
+                Player.Left += speed;
+            }
+            if (MoveUp == true && Player.Top > 0)
+            {
+                Player.Top -= speed;
+            }
+            if (MoveDown == true && Player.Bottom < 497)
+            {
+                Player.Top += speed;
+            }
+        }
+        //Method
+        private void Keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                MoveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                MoveRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                MoveUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                MoveDown = true;
+            }
+        }
+
+        private void Keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                MoveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                MoveRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                MoveUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                MoveDown = false;
+            }
+        }
+    }
+}
